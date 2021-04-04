0d48382993b374f836af72a6210ff8581b5f431d
Nebuchaddy <nebuchaddy@gmail.com>
2021-04-04 17:02:20 +0200

    Inputs
    - Added Input Manager
    - Added Input System & mappings
    - Movement and camera
    - Full controller support

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -1,49 +1,81 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    [Header("Data")]
+    [Header("Data")] //These can be set up in the Inspector to personalize the player
     public float lookSens = 100f;
+    public float lookClamp = 85f;
+    public float moveSpeed = 10f;
+    public float airMoveDivider = 2f;
+    public float jumpHeight = 2.5f;
+    public float gravity = -9.81f;
+    public bool invertLook = false;
+    [SerializeField] LayerMask playerLayer;
 
-    [Header("Internal Data")]
+    [Header("Internal Data")] //These are internal variables that are automatically set. Useful for debug.
     [SerializeField] GameObject player;
+    [SerializeField] CharacterController playerController;
     [SerializeField] Camera mainCamera;
-    [SerializeField] float xRot = 0f;
+    [SerializeField] Vector2 moveInput;
+    [SerializeField] Vector2 lookInput;
+    [SerializeField] Vector3 vertVel = Vector3.zero;
+    [SerializeField] float xRot = 0;
+    [SerializeField] bool isGrounded;
+    [SerializeField] bool isJumping;
+    [SerializeField] Inputs inputs;
+    [SerializeField] Inputs.MovementActions inputMovement;
 
+    #region Unity Methods
     void Awake() => SetUp();
 
     void Update()
-    {
-        HandleLookInputMouse();
-        HandleLookInputController();
-
-        Debug.Log(Input.GetAxis("Horizontal"));
+    {       
+        HandleMovement(moveInput);
+        HandleLook(lookInput);
+        HandleJump();
     }
 
-    void HandleLookInputMouse()
+    void OnEnable() => inputs.Enable();
+
+    void OnDisable() => inputs.Disable();
+    #endregion
+
+    #region Custom Methods
+    void HandleMovement(Vector2 moveInp)
     {
-        float lookX = Input.GetAxis("Mouse X") * lookSens * Time.deltaTime;
-        float lookY = Input.GetAxis("Mouse Y") * lookSens * Time.deltaTime;
-
-        player.transform.Rotate(Vector3.up * lookX);
-        xRot -= lookY;
-        xRot = Mathf.Clamp(xRot, -85f, 85f);
-
-        mainCamera.transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        //Compact *if* for slower air movement
+        Vector3 horVel = isGrounded ? (player.transform.right * moveInp.x + player.transform.forward * moveInp.y) * moveSpeed : (player.transform.right * moveInp.x + player.transform.forward * moveInp.y) * moveSpeed / airMoveDivider;
+        playerController.Move(horVel * Time.deltaTime);
+        playerController.Move(vertVel * Time.deltaTime);
+        //Groundcheck and gravity calcs
+        isGrounded = Physics.CheckSphere(new Vector3(player.transform.position.x, player.transform.position.y + 0.35f, player.transform.position.z), 0.4f, ~playerLayer);
+        vertVel.y = isGrounded && !isJumping ? 0f : vertVel.y += gravity * Time.deltaTime;
     }
 
-    void HandleLookInputController()
+    void HandleLook(Vector2 lookInp)
     {
-        float lookX = Input.GetAxis("RightStick X") * lookSens * Time.deltaTime;
-        float lookY = Input.GetAxis("RightStick Y") * lookSens * Time.deltaTime;
+        //Sets look axis
+        float lookX = lookInp.x * lookSens * Time.deltaTime;
+        float lookY = lookInp.y * lookSens * Time.deltaTime;
+        //Clamps vertical axis
+        xRot = invertLook ? xRot += lookY : xRot -= lookY;
+        xRot = Mathf.Clamp(xRot, -lookClamp, lookClamp);
+        //Moves camera and player
+        mainCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        player.transform.Rotate(Vector3.up, lookX);
+    }
 
-        player.transform.Rotate(Vector3.up * lookX);
-        xRot -= lookY;
-        xRot = Mathf.Clamp(xRot, -85f, 85f);
-
-        mainCamera.transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+    void HandleJump()
+    {
+        if (isGrounded && inputMovement.Jump.triggered)
+        {
+            isJumping = true;
+            vertVel.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity); //Math function for jumping
+        }
+        else isJumping = false;
     }
 
     void ToggleCursor() => Cursor.visible = !Cursor.visible;
@@ -54,13 +86,18 @@ public class InputManager : MonoBehaviour
         GameObject[] objs = GameObject.FindGameObjectsWithTag("InputManager");
         if (objs.Length > 1) Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
-
+        //Input Setup
+        inputs = new Inputs();
+        inputMovement = inputs.Movement;       
+        inputMovement.Movement.performed += _ => moveInput = _.ReadValue<Vector2>();
+        inputMovement.Look.performed += _ => lookInput = _.ReadValue<Vector2>();
         //Data SetUp
         player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<CharacterController>();
         mainCamera = Camera.main;
-
         //Cursor SetUp
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
     }
+    #endregion
 }
---
+++ Assets/Scripts/Input/Inputs.cs
@@ -0,0 +1,267 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/Inputs.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Inputs : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Inputs()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Inputs"",
+    ""maps"": [
+        {
+            ""name"": ""Movement"",
+            ""id"": ""29e553ff-04be-4b26-b40e-3cd741495b87"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""7df5e354-53ea-41da-b78d-9a66c30edd81"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""ff0e1cd7-aca3-47a7-b4aa-9ed5001ffa30"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""abc8f859-4eda-452c-9e2d-7045ae0d675b"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""38104de2-7d50-439d-aff2-6c4d0510af49"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""e96d6937-6c3c-4153-bde5-c53a2273e3de"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""ec8f9b29-1832-4df0-a897-df212c4d8d13"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""84485b48-b475-4afe-b266-9a97cbe16f7f"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""a5b3c859-5d97-4240-bc65-c447e3b1afac"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""962999c0-d750-47c0-b1a4-38eb92aa91b0"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": ""NormalizeVector2"",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""43a219b9-1596-433d-91cf-91e1cae7e821"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""92f5576a-a4b4-448b-baa1-860958617676"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a8b46d46-e4e3-422a-90df-a7ed03191caa"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""60e3912d-9e62-4dd0-87ae-cf35761daa27"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Movement
+        m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
+        m_Movement_Movement = m_Movement.FindAction("Movement", throwIfNotFound: true);
+        m_Movement_Jump = m_Movement.FindAction("Jump", throwIfNotFound: true);
+        m_Movement_Look = m_Movement.FindAction("Look", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Movement
+    private readonly InputActionMap m_Movement;
+    private IMovementActions m_MovementActionsCallbackInterface;
+    private readonly InputAction m_Movement_Movement;
+    private readonly InputAction m_Movement_Jump;
+    private readonly InputAction m_Movement_Look;
+    public struct MovementActions
+    {
+        private @Inputs m_Wrapper;
+        public MovementActions(@Inputs wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Movement_Movement;
+        public InputAction @Jump => m_Wrapper.m_Movement_Jump;
+        public InputAction @Look => m_Wrapper.m_Movement_Look;
+        public InputActionMap Get() { return m_Wrapper.m_Movement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MovementActions set) { return set.Get(); }
+        public void SetCallbacks(IMovementActions instance)
+        {
+            if (m_Wrapper.m_MovementActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
+                @Look.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnLook;
+            }
+            m_Wrapper.m_MovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+            }
+        }
+    }
+    public MovementActions @Movement => new MovementActions(this);
+    public interface IMovementActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+    }
+}
